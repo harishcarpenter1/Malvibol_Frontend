bd8a07b08ee36caf32a8278e121d7222fe22d3ce
harishcarpenter1 <harishcarpenter123@gmail.com>
2024-03-24 15:12:42 +0530

    first commit

---
+++ src/components/test/image.js
@@ -0,0 +1,7 @@
+import fox from "./images/Malva.jpg";
+import jill from "./images/PashupatiNath.jpg";
+import cha1 from "./images/HarsidhiMata.jpg";
+import cha2 from "./images/Mahakal.jpg";
+
+
+export default [fox, jill, cha1, cha2];
